a90c32afd76b5fda2328bcfbfd50009b6aec0ac5
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2025-04-18 17:16:41 +0100

    add toast notifications

--- src/mcp_windows/main.py
+++ src/mcp_windows/main.py
@@ -1,10 +1,11 @@
 from fastmcp import FastMCP
 
 from mcp_windows.media import mcp as media_mcp
+from mcp_windows.notifications import mcp as notifications_mcp
 
 mcp: FastMCP = FastMCP(
     name="windows",
 )
 
 mcp.mount("media", media_mcp, tool_separator=".")
-
+mcp.mount("notifications", notifications_mcp, tool_separator=".")
